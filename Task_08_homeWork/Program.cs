71e46be8fd7981aec74dbe207502281ffca9841f
Dmitri5 <dmitriprogram@gmail.com>
2022-07-02 12:56:07 +0700

    Added Task 08 Home Work

---
+++ Task_08_homeWork/Program.cs
@@ -0,0 +1,21 @@
+﻿// 8. Напишу программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+// -------------------------------------------------------------------------
+
+Console.WriteLine("Эта программа принимает на вход число (N), а на выходе показывает \nвсе чётнык числа от 1 до N.");
+Console.Write("Введите число и нажмите Enter: ");
+char simvol = ','; 
+int number = int.Parse(Console.ReadLine());
+for (int index = 1; index <= number; index++) if (index %2 == 0) 
+{
+    Console.Write(index);
+    if (index == number) break;
+    Console.Write(simvol);
+    Console.Write(" ");
+    
+}
+
+
+
