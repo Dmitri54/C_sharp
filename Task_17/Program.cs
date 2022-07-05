66673e7af103e44f950d45110be0dad84bd72eb1
Dmitri5 <dmitriprogram@gmail.com>
2022-07-05 16:35:17 +0700

    Added Task 17 dicision through if_else

---
+++ Task_17/Program.cs
@@ -0,0 +1,14 @@
+﻿/* 17. Напишу программу, которая принимает на вход координаты точки (X и Y),
+причём X = 0 и Y = 0 и выдаёт номер четверти плоскости, в которой находится эта точка */
+
+Console.WriteLine("Введите координаты точки (X и Y)");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) Console.WriteLine("Первая четверть");
+else if (x < 0 && y > 0) Console.WriteLine("Вторая четверть");
+else if (x < 0 && y < 0) Console.WriteLine("Третья четверть");
+else if (x > 0 && y < 0) Console.WriteLine("Четвертая четверть");
+else Console.WriteLine("Введены не корректные данные");
