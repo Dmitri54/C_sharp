b24197359a516564a02a38ba1bdabd91d36c2c7e
Dmitri5 <dmitriprogram@gmail.com>
2022-07-05 16:55:35 +0700

    Added Task 17 with method

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -7,8 +7,39 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y: ");
 int y = Convert.ToInt32(Console.ReadLine());
 
-if (x > 0 && y > 0) Console.WriteLine("Первая четверть");
-else if (x < 0 && y > 0) Console.WriteLine("Вторая четверть");
-else if (x < 0 && y < 0) Console.WriteLine("Третья четверть");
-else if (x > 0 && y < 0) Console.WriteLine("Четвертая четверть");
-else Console.WriteLine("Введены не корректные данные");
+// Первый вариант решения.
+// -----------------------------------------------------------------------------------
+// if (x > 0 && y > 0) Console.WriteLine("Первая четверть");
+// else if (x < 0 && y > 0) Console.WriteLine("Вторая четверть");
+// else if (x < 0 && y < 0) Console.WriteLine("Третья четверть");
+// else if (x > 0 && y < 0) Console.WriteLine("Четвертая четверть");
+// else Console.WriteLine("Введены не корректные данные");
+
+// -----------------------------------------------------------------------
+// Решение методом.
+
+// string Quarter(int xc, int yc)
+// {
+//     string result = ""; // Что бы вернуть строку, создам переменную (пустая строка) в которую буду записывать значение.
+//     if (x > 0 && y > 0) result = "Первая четверть";
+//     else if (x < 0 && y > 0) result = "Вторая четверть";
+//     else if (x < 0 && y < 0) result = "Третья четверть";
+//     else if (x > 0 && y < 0) result = "Четвертая четверть";
+//     else result = "Введены не корректные данные";
+//     return result;
+// }
+// ---------------------------------------------------------------------------
+// Переписал код без создания переменной.
+string Quarter(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return "Первая четверть";
+    if (xc < 0 && yc > 0) return "Вторая четверть";
+    if (xc < 0 && yc < 0) return "Третья четверть";
+    if (xc > 0 && yc < 0) return "Четвертая четверть";
+    return "Введены не корректные данные";
+}
+
+string result = Quarter(x, y);
+string result2 = Quarter(9, 1);
+Console.WriteLine(result);
+Console.WriteLine(result2);
