6222b1f0943268108ea996f4cfdeed915631ee51
Dmitri5 <dmitriprogram@gmail.com>
2022-07-05 16:01:25 +0700

    Found a mistake, fixed it

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -6,13 +6,6 @@
 Console.Write("Программа выдает название дня недели по заданному номеру.\nВведите цифру от 1 до 7 и нажмите Enter: ");
 // Чтобы перенести текст на следующую строку пишу "\n".
 int day = Convert.ToInt32(Console.ReadLine());
-if (day == 1) Console.WriteLine("Понедельник");
-else if (day == 2) Console.WriteLine("Вторник");
-else if (day == 3) Console.WriteLine("Среда");
-else if (day == 4) Console.WriteLine("Четверг");
-else if (day == 5) Console.WriteLine("Пятница");
-else if (day == 6) Console.WriteLine("Суббота");
-else if (day == 7) Console.WriteLine("Воскресенье");
 else Console.WriteLine("Это не цифра от 1 до 7");
 
 // -----------------------------------------------------------------------------------------------
