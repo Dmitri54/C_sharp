599d8d05f1c37e9619b7ba1efb330e4f07c3daa1
Dmitri5 <dmitriprogram@gmail.com>
2022-07-07 16:00:19 +0700

    Added Task 28

---
+++ Task_28/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 28. Напишу программу, которая принимает на вход число N
+ и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120 */
+
+Console.WriteLine("Программа выдает произведение чисел от 1 до N.");
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Count (int number)
+{
+    int x = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        x = i * x;
+    }    
+    return x; 
+    
+}
+
+int result = Count(num);
+Console.Write(result);
+
