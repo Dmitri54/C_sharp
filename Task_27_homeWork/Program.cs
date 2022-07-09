ec68d073a7a9a60a6a445e4dd92aee23fe3cb087
Dmitri5 <dmitriprogram@gmail.com>
2022-07-09 17:42:29 +0700

    Added Task 27

---
+++ Task_27_homeWork/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 27. Напишу программу, которая принимает на вход число
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.WriteLine("Программа выдаёт сумму цифр в числе.");
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    int remainder = 0;
+    while (num != 0)
+    {
+        remainder += num % 10;
+        num = num / 10;
+    }
+    return remainder;
+}
+int result = SumNumbers(number);
+Console.WriteLine($"Сумма чисел числа {number} -> {result}");
+
