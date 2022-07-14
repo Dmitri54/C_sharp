1d305c264038b7a0ebf4e71179865f61d1520311
Dmitri5 <dmitriprogram@gmail.com>
2022-07-14 21:45:12 +0700

    Added comments

---
+++ Task_44/Program.cs
@@ -0,0 +1,28 @@
+﻿/* 44. Не используя рекурсию, выведите первые N чисел
+Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8 */
+// ---------------------------------------------------------------------------------------
+Console.Write("Укажите количество чисел Фибоначчи для вывода: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// f(n) = f(n-2) + f(n-1)
+int first = 0;
+int second = 1;
+int counter = 0;
+
+if (N = 1) Console.Write("0");
+if (N = 2) Console.Write("1");
+else
+{
+    int temp1 = 0;
+    int temp2 = 1;
+    int result = 0;
+    while (counter < N)
+    {
+        result = temp1 = temp2;
+        Console.Write();
+    }
+}
+
---
+++ Task_45/Program.cs
@@ -0,0 +1,40 @@
+﻿/* 45. Напишу программу, которая будет создавать
+копию заданного массива с помощью поэлементного
+копирования. */
+// -------------------------------------------------------------------------------------
+int[] GreateArrayRndInt(int size, int begin, int end) // Метод - создает и заполняет массив элементами.
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(begin, end + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // Метод - напечатает массив в консоль.
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i] + "]");
+    }
+}
+
+int[] CopyArray(int[] array) // Метод - создаст копию массива с помощью поэлементного копирования.
+{
+    int[] copy = new int[array.Length];
+    for (int i = 0; i < copy.Length; i++)
+    {
+        copy[i] = array[i];
+    }
+    return copy;
+}
+
+int[] massive = GreateArrayRndInt(5, 1, 10);
+PrintArray(massive);
+int[] massiveCopy = CopyArray(massive);
+Console.WriteLine();
+PrintArray(massiveCopy);
