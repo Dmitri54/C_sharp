6144173a72aedea96d8ec280cf91dab88e066fe7
Dmitri5 <dmitriprogram@gmail.com>
2022-07-24 21:12:06 +0700

    Added Task 56 home Work

---
+++ Task_56_homeWork/Program.cs
@@ -0,0 +1,77 @@
+﻿/* 56. Задам прямоугольный двумерный массив. 
+Напишу программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+с наименьшей суммой элементов: 1 строка */
+// ----------------------------------------------------------------------------------------
+int[,] GreateMatrixRndInt(int row, int col, int diap) // Метод, создает и заполняет двумерный массив с Rnd элементами.
+{
+    int[,] array = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(diap + 1);
+        }
+
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array) // Этот метод напечатает двумерный массив в консоль.
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3}, ");
+            else Console.Write($"{array[i, j],3}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumLineElements(int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+
+Console.Write("Введите количетво строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количетво столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите диапазона: от 1 до ");
+int range = Convert.ToInt32(Console.ReadLine());
+
+int[,] arrayMatrix = GreateMatrixRndInt(m, n, range);
+Console.WriteLine($"Сгенерирован двумерный массив:");
+PrintMatrix(arrayMatrix);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(arrayMatrix, 0);
+for (int i = 1; i < arrayMatrix.GetLength(0); i++)
+{
+    int tempSumLine = SumLineElements(arrayMatrix, i);
+    if (sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
+}
+
+Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой элементов = {sumLine}");
+
+// Работает!
