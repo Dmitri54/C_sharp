c960a1702416b78d1d459b1df2f9b27b64297600
Dmitri5 <dmitriprogram@gmail.com>
2022-07-30 21:44:04 +0700

    Added Task 65 Recursion sum elements number

---
+++ Task_67/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 67. Напишу программу, которая 
+будет принимать на вход число и
+возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+Console.Clear();
+Console.Write("Задайте число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = NaturalNumberSum(num, 0);
+Console.WriteLine(sum);
+
+int sum2 = NaturalNumberSumVar2(num);
+Console.WriteLine(sum2);
+
+int NaturalNumberSum(int num, int sum)
+{
+    sum += num % 10;
+    if (num / 10 == 0) return sum;
+    else return NaturalNumberSum(num / 10, sum);
+}
+
+// Второй вариант, преподователя.
+int NaturalNumberSumVar2(int num)
+{
+    if (num > 0) return num % 10 + NaturalNumberSumVar2(num / 10);
+    return 0;
+}
+
+
