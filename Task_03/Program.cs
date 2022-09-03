c2bee8a7e5080f9ab7b6dba80400bc95ecda73b1
Dmitri5 <dmitriprogram@gmail.com>
2022-09-03 16:09:34 +0700

    Correct code Task 03

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -5,20 +5,20 @@
 // -----------------------------------------------------------------------------------------------
 Console.Write("Программа выдает название дня недели по заданному номеру.\nВведите цифру от 1 до 7 и нажмите Enter: ");
 // Чтобы перенести текст на следующую строку пишу "\n".
-int day = Convert.ToInt32(Console.ReadLine());
+int day = Convert.ToInt32(Console.ReadLine());_
 else Console.WriteLine("Это не цифра от 1 до 7");
 
 // -----------------------------------------------------------------------------------------------
-// switch(day)
-// // switch() - это оператор выбора обеспечивает многонаправленное ветвление в программе. 
-// // Он позволяет сделать выбор среди нескольких альтернативных вариантов дальнейшего выполнения программы. 
-// {
-//     case 1: Console.WriteLine("Понедельник"); break;
-//     case 2: Console.WriteLine("Вторник"); break;
-//     case 3: Console.WriteLine("Среда"); break;
-//     case 4: Console.WriteLine("Четверг"); break;
-//     case 5: Console.WriteLine("Пятница"); break;
-//     case 6: Console.WriteLine("Суббота"); break;
-//     case 7: Console.WriteLine("Воскресенье"); break;
-//     default: Console.WriteLine("Это не цифра от 1 до 7"); break;
-// }
+switch(day)
+// switch() - это оператор выбора обеспечивает многонаправленное ветвление в программе. 
+// Он позволяет сделать выбор среди нескольких альтернативных вариантов дальнейшего выполнения программы. 
+{
+    case 1: Console.WriteLine("Понедельник"); break;
+    case 2: Console.WriteLine("Вторник"); break;
+    case 3: Console.WriteLine("Среда"); break;
+    case 4: Console.WriteLine("Четверг"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота"); break;
+    case 7: Console.WriteLine("Воскресенье"); break;
+    default: Console.WriteLine("Это не цифра от 1 до 7"); break;
+}
